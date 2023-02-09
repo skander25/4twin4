0bc9c6a3fb18cb876f820e5f76f858b1f1f391d2
Skander Mazgar <skander.mazgar@esprit.tn>
2023-02-09 10:56:45 +0100

    atelier1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import hello from './greeting.js';
+function App() {
+  return (
+    <div className="App">
+     <hello></hello>
+    </div>
+  );
+}
+
+export default App;
